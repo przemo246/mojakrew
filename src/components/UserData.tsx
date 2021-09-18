48bdcbe2fbd6de3fcbf79602b2a57baed63db8cc
Przemek <przemo247@outlook.com>
2021-09-18 20:01:50 +0200

    Add markup in UserData component

--- src/components/UserData.tsx
+++ src/components/UserData.tsx
@@ -1,5 +1,30 @@
 import { FunctionComponent } from "react";
 
 export const UserData: FunctionComponent = () => {
-  return <div></div>;
+  return (
+    <section className="user-data">
+      <span className="order-number">1</span>
+      <h1 className="heading-primary">Uzupełnij informacje o badaniu</h1>
+      <form className="user-data__form">
+        <label htmlFor="test-date" className="red-label">
+          data
+        </label>
+        <input type="date" name="test-date" id="test-date" />
+        <label htmlFor="test-location" className="red-label">
+          nazwa i adres placówki
+        </label>
+        <input
+          type="text"
+          name="test-location"
+          placeholder="np. Bruss, Aleja Grunwaldzka 60, Gdańsk"
+        />
+        <button type="submit" className="btn btn-red">
+          OK
+        </button>
+        <button type="reset" className="btn btn-red">
+          Wyczyść
+        </button>
+      </form>
+    </section>
+  );
 };
