a0d5d641e482e4e24560011ea1af41e687aff1f0
Przemek <przemo247@outlook.com>
2021-09-18 19:37:47 +0200

    Add main app markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
 import "./scss/main.scss";
+import logo from "./img/logo1x.png";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <div className="container">
+      <nav className="navigation">
+        <div className="logo-container">
+          <img src={logo} className="logo" alt="" />
+        </div>
+        <ul className="navigation__list"></ul>
+        <li className="navigation__item">BADANIA</li>
+        <li className="navigation__item">PRZEWODNIK</li>
+        <li className="navigation__item">KONTAKT</li>
+      </nav>
+      <section className="user-data"></section>
+      <section className="user-results"></section>
+    </div>
+  );
 };
