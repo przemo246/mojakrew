2ce577d4bf5e17263279874727b03e3c7e852aea
Przemek <przemo247@outlook.com>
2021-09-21 22:13:01 +0200

    Display BloodElement component on UI

--- src/components/UserAnalysis/UserAnalysis.tsx
+++ src/components/UserAnalysis/UserAnalysis.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from "react";
 import { Test } from "../../../types/interfaces";
+import { BloodElement } from "./BloodElement";
 
 interface UserAnalysisProps {
   currentTest: Test | null;
@@ -17,11 +18,11 @@ export const UserAnalysis: FunctionComponent<UserAnalysisProps> = ({
         <h2 className="heading-secondary">WART. REFERENCYJNA</h2>
       </div>
       <div className="elements-list">
-        <div className="blood-element">
-          <div className="blood-element__element-name">
-            {/* <span>IG</span><div className="query-box"><span>?</span><div className="details-popup">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, dolorum. Quibusdam suscipit, eos voluptate accusantium amet reiciendis sequi provident, quae facere asperiores obcaecati quasi ullam impedit, dolorem iure cumque optio!Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, dolorum. Quibusdam suscipit, eos voluptate accusantium amet reiciendis sequi provident, quae facere asperiores obcaecati quasi ullam impedit, dolorem iure cumque optio!</div></div></div><div className="blood-element__result user-pass"><span>10</span><svg className="icon-medium margin-left-small"><use xlink:href="img/sprites.svg#icon-checkmark"></use></svg></div><div className="blood-element__unit"><span>G/l</span></div><div className="blood-element__reference"><span>10 - 11</span><div className="blood-element__delete-icon"><a id="btn-delete"><svg className="icon-medium delete-icon"><use xlink:href="img/sprites.svg#icon-close"></use></svg></a></div> */}
-          </div>
-        </div>
+        {currentTest?.elements
+          ? currentTest.elements.map((element) => (
+              <BloodElement key={element.id} element={element} />
+            ))
+          : null}
       </div>
     </section>
   );
