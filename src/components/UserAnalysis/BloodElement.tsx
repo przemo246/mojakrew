736a5bdf443c00d59c46e1ec7563511e8dc50877
Przemek <przemo247@outlook.com>
2021-09-23 01:01:01 +0200

    Display blood elements on UI

--- src/components/UserAnalysis/BloodElement.tsx
+++ src/components/UserAnalysis/BloodElement.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from "react";
 import { Element } from "../../../types/interfaces";
+import { bloodElements } from "../../ts/bloodElements";
 
 interface BloodElementProps {
   element: Element;
@@ -9,22 +10,30 @@ export const BloodElement: FunctionComponent<BloodElementProps> = ({
   element,
 }) => {
   return (
-    <div className="blood-element">
+    <div className="blood-element" id={element.id}>
       <div className="blood-element__element-name">
-        <span>IG</span>
+        <span>{element.name}</span>
         <div className="query-box">
           <span>?</span>
         </div>
       </div>
-      <div className="blood-element__result user-pass">
-        <span>10</span>
-        {/*  */}
+      <div
+        className={`blood-element__result ${
+          element.result >= element.referenceFrom &&
+          element.result <= element.referenceTo
+            ? "user-pass"
+            : "user-fail"
+        }`}
+      >
+        <span>{element.result}</span>
       </div>
       <div className="blood-element__unit">
-        <span>G/l</span>
+        <span>{bloodElements[element.id].unit}</span>
       </div>
       <div className="blood-element__reference">
-        <span>10 - 11</span>
+        <span>
+          {element.referenceFrom} - {element.referenceTo}
+        </span>
         <div className="blood-element__delete-icon"> {/* Delete icon */}</div>
       </div>
     </div>
