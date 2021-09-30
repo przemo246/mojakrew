3abf471f7981b0e4922ea9675d0c0faff59de595
Przemek <przemo247@outlook.com>
2021-09-30 23:23:25 +0200

    Add dropdown menu

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,6 +1,6 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import reactDom from "react-dom";
-import { FaTimes } from "react-icons/fa";
+import { FaTimes, FaEllipsisV } from "react-icons/fa";
 import { Test } from "../../../types/interfaces";
 
 interface ModalProps {
@@ -14,7 +14,11 @@ export const Modal: FunctionComponent<ModalProps> = ({
   onClose,
   tests,
 }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   if (!open) return null;
+  const toggleDropdownMenu = () => {
+    setIsDropdownOpen((prev) => !prev);
+  };
   const modalRoot = document.getElementById("modal-root") as HTMLElement;
   return reactDom.createPortal(
     <>
@@ -25,6 +29,12 @@ export const Modal: FunctionComponent<ModalProps> = ({
         </div>
         <div className="modal__content">
           <h2 className="modal__heading">Badania</h2>
+          <div
+            className="dropdown"
+            style={isDropdownOpen ? { display: "block" } : { display: "none" }}
+          >
+            <p>Dropdown</p>
+          </div>
           <div className="user-tests">
             {tests.length > 0 ? (
               tests.map((test, i) => (
@@ -33,14 +43,18 @@ export const Modal: FunctionComponent<ModalProps> = ({
                   key={test.id}
                   id={test.id.toString()}
                 >
-                  <span className="red-label">{i + 1}. </span>
-                  <span>
-                    Badanie z dnia{" "}
-                    {new Date(test.date).toLocaleDateString("en-GB")} w{" "}
-                    {test.location}
-                  </span>
-                  {/* <div className="user-tests__options">
-            </div> */}
+                  <div className="user-tests__description">
+                    <span className="red-label">{i + 1}. </span>
+                    <span>
+                      Badanie z dnia{" "}
+                      {new Date(test.date).toLocaleDateString("en-GB")} w{" "}
+                      {test.location}
+                    </span>
+                  </div>
+                  <FaEllipsisV
+                    className="options-icon"
+                    onClick={toggleDropdownMenu}
+                  />
                 </div>
               ))
             ) : (
