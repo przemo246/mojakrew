7ed613a6783aa15b57009dbeedbc1d4a27c13f7a
Przemek <przemo247@outlook.com>
2021-12-28 12:29:18 +0100

    Remove unused dependency

--- src/components/UserAnalysis/UserAnalysis.tsx
+++ src/components/UserAnalysis/UserAnalysis.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import { FunctionComponent } from "react";
 import { Test } from "../../../types/interfaces";
 import { BloodElement } from "./BloodElement";
 
